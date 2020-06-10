ab54fc9d7e9e091ddd5ecdb025184da06a07689f
sandy-tartari <62676992+sandy-tartari@users.noreply.github.com>
2020-06-10 17:35:43 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Algoritmo 1 = leia o nome e escreva um comprimento (Hello) para este nome
+            string nome;
+            Console.WriteLine("digite o seu nome");
+            nome = Console.ReadLine();
+            Console.WriteLine("Hello {0}!", nome);
+        }
+    }
+}
